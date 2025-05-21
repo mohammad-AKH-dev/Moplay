003afed305709dc80e44d0b841044a28828206e7
mohammad <mohammadakhlaghi843@gmail.com>
2025-05-21 18:47:53 +0200

    prepared first configs

---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+ 
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        'regular': 'Ubuntu-Regular'
+      },
+      colors: {
+        'link': '#29B6F6',
+        'dark-title': '#ffffff',
+        'title': '#0A0D14',
+        'selected': '#29B6F6',
+        'red': '#FF5858',
+        'footer': '#757F95'
+      },
+      backgroundColor: {
+       'main': '#06090f'
+      }
+    },
+  },
+  plugins: [],
+}
