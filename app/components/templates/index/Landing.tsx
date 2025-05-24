48a43cb8a1be01ea752ad0c501016a0455bf9357
mohammad <mohammadakhlaghi843@gmail.com>
2025-05-24 21:33:43 +0200

    swiper initialed in landing

---
+++ app/components/templates/index/Landing.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/pagination';
+import { Pagination } from 'swiper/modules';
+
+function Landing() {
+  return (
+    <div className='landing'>
+        <Swiper pagination={true} modules={[Pagination]} className="mySwiper">
+        <SwiperSlide>Slide 1</SwiperSlide>
+        <SwiperSlide>Slide 2</SwiperSlide>
+        <SwiperSlide>Slide 3</SwiperSlide>
+        <SwiperSlide>Slide 4</SwiperSlide>
+        <SwiperSlide>Slide 5</SwiperSlide>
+        <SwiperSlide>Slide 6</SwiperSlide>
+        <SwiperSlide>Slide 7</SwiperSlide>
+        <SwiperSlide>Slide 8</SwiperSlide>
+        <SwiperSlide>Slide 9</SwiperSlide>
+      </Swiper>
+    </div>
+  )
+}
+
+export default Landing
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import Navbar from "./components/modules/Navbar/Navbar";
+import Landing from "./components/templates/index/Landing";
 
 export default function Home() {
   return (
    <div className="font-regular text-[30px] p-8">
       <Navbar/>
+      <Landing/>
    </div>
   );
 }
