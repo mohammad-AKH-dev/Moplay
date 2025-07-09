450690d6cb001e1ea548aca5dc424ed460e9b392
Mohammad (FrontEnd Developer) <mohammadakhlaghi843@gmail.com>
2025-07-09 10:41:20 +0330

    some issue styles fixed

--- app/components/templates/single-tvShow/SeasonDetails.tsx
+++ app/components/templates/single-tvShow/SeasonDetails.tsx
@@ -26,7 +26,7 @@ function SeasonDetails(props: showSeasonType) {
       <input type="checkbox" id="my_modal_7" className="modal-toggle" />
       <div className="modal -top-[160px] sm:-top-[60px] lg:top-0" role="dialog">
         <div
-          className="modal-box  text-center sm:text-left
+          className="modal-box bg-white dark:bg-base-100  text-center sm:text-left
          -translate-y-[20.5rem] lg:-translate-y-[5.5rem] grid grid-cols-1 gap-y-4 sm:gap-y-0 sm:grid-cols-2 lg:min-w-[800px]"
         >
           <img
